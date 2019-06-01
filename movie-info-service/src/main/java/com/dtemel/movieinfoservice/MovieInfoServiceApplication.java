0f7ae7d3c99a5d0a18b4ac65877d1da2cb5245a3
Taner TEMEL <tanertemel@outlook.com>
2019-06-01 13:03:34 +0300

    commit

--- movie-info-service/src/main/java/com/dtemel/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/dtemel/movieinfoservice/MovieInfoServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class MovieInfoServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
